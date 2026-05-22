549c728e7d89dec76064f9adabffad4557179b50
leis33 <l.abdulah@abv.bg>
2026-05-22 18:50:25 +0300

    fix: make Main app to use Spring

---
+++ src/main/java/nbu/springProject/EmrApplication.java
@@ -0,0 +1,11 @@
+package nbu.springProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmrApplication.class, args);
+    }
+}
